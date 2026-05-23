7764aac749460e4f5704d282c87ed154cee132a1
NyanPhyo <nyanphyo376@gmail.com>
2026-05-23 19:16:21 +0800

    Add files via upload

---
+++ process.php
@@ -0,0 +1,16 @@
+
+
+<?php
+    include 'header.inc';
+    session_start();
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if($username == "admin" && $password == "password123") {
+        $_SESSION["user"] = $username;
+        header("Location:welcome.php");
+    } else{
+        echo "Invalid login. <a href='login.php'>Try again</a>";
+    }
+    include 'footer.inc';
+?>
---
+++ welcome.php
@@ -0,0 +1,11 @@
+
+<?php
+    include "header.inc";
+    session_start();
+    if(isset($_SESSION["user"])) {
+        echo "Welcome, ".$_SESSION["user"];
+    }else {
+        header("Location: login.php");
+    }
+    include "footer.inc";
+?>
